7d08b6797ffb5a08e6edaa25f178878c9337b5ac
Kevin Smith <ksmith0813@gmail.com>
2022-10-08 11:27:26 -0500

    added more config defaults, added __test__ and jest setup file

---
+++ __tests__/jest.setup.ts
@@ -0,0 +1,43 @@
+import { configure } from '@testing-library/react';
+import { useSession } from 'next-auth/react';
+
+configure({ testIdAttribute: 'id' });
+
+jest.mock('next-auth/react');
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+export const getMockSessionData = (loggedIn?: boolean) => {
+  const status = loggedIn ? 'authenticated' : 'unauthenticated';
+  const data = loggedIn
+    ? {
+        user: {
+          name: loggedIn ? 'testing' : '',
+          email: loggedIn ? 'testing@email.com' : '',
+        },
+      }
+    : null;
+
+  return {
+    status: status,
+    data: data,
+  };
+};
+
+export const createMockSession = (loggedIn?: boolean) => {
+  return (useSession as jest.Mock).mockReturnValue(getMockSessionData(loggedIn));
+};
--- pages/index.ts
+++ pages/index.ts
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
 
-export default function Home() {
+export const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -51,7 +50,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
