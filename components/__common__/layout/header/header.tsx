5682c629aa5fc02ab9b126266afac21e0cc0ee37
Kevin Smith <ksmith0813@gmail.com>
2022-10-15 13:11:10 -0500

    added logic to set current route styling

--- components/__common__/layout/header/header.tsx
+++ components/__common__/layout/header/header.tsx
@@ -1,30 +1,46 @@
 import { ROUTES, STRINGS } from 'constants/global';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { AvatarLogo } from './avatarLogo';
 import { Logo } from './logo';
 
-export const Header = () => (
-  <div className='header-container' id='header'>
-    <div>
-      <Logo />
+export const Header = () => {
+  const router = useRouter();
+  const { pathname } = router;
+
+  const routeClass = (route: string) => `${pathname === route ? 'current-route' : ''}`;
+
+  return (
+    <div className='header-container' id='header'>
+      <div>
+        <Logo />
+      </div>
+      <div className='header'>
+        <div className={routeClass(ROUTES.home)}>
+          <Link href={ROUTES.home} id='home-link'>
+            Home
+          </Link>
+        </div>
+        <div className={routeClass(ROUTES.media)}>
+          <Link href={{ pathname: ROUTES.media, query: { q: STRINGS.search } }} id='media-link'>
+            Media
+          </Link>
+        </div>
+        <div className={routeClass(ROUTES.weather)}>
+          <Link href={{ pathname: ROUTES.weather, query: { q: STRINGS.search } }} id='weather-link'>
+            Weather
+          </Link>
+        </div>
+        <div className={routeClass(ROUTES.people)}>
+          <Link href={{ pathname: ROUTES.people, query: { q: STRINGS.search } }} id='people-link'>
+            People
+          </Link>
+        </div>
+      </div>
+      <div>
+        <AvatarLogo />
+      </div>
     </div>
-    <div className='header'>
-      <Link href={ROUTES.home} id='home-link'>
-        Home
-      </Link>
-      <Link href={{ pathname: ROUTES.media, query: { q: STRINGS.search } }} id='media-link'>
-        Media
-      </Link>
-      <Link href={{ pathname: ROUTES.weather, query: { q: STRINGS.search } }} id='weather-link'>
-        Weather
-      </Link>
-      <Link href={{ pathname: ROUTES.people, query: { q: STRINGS.search } }} id='people-link'>
-        People
-      </Link>
-    </div>
-    <div>
-      <AvatarLogo />
-    </div>
-  </div>
-);
+  );
+};
