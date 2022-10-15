05f292143d23906b5083d567829eba33a1cb5c4e
Kevin Smith <ksmith0813@gmail.com>
2022-10-15 12:58:11 -0500

    replaced external link buttons with brand svg's, updated hero styling

--- components/__common__/layout/hero/hero.tsx
+++ components/__common__/layout/hero/hero.tsx
@@ -1,1 +1,9 @@
-export const Hero = () => <div className='hero'>Next.js | SWR | GraphQL | tailwind.css</div>;
+import Image from 'next/image';
+import BackgroundImage from 'public/web.png';
+
+export const Hero = () => (
+  <div className='hero-container'>
+    <Image src={BackgroundImage} className='hero-image' alt='Hero' layout='fill' priority id='site-logo' />
+    <div className='hero'>Next.js | SWR | GraphQL | tailwind.css</div>
+  </div>
+);
--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Page } from 'components/__common__/layout/page/page';
-import { Button } from 'antd';
+import Image from 'next/image';
+import GraphQLIcon from 'public/graphql.svg';
+import SWRIcon from 'public/swr.svg';
+import TailwindIcon from 'public/tailwind.svg';
+import VercelIcon from 'public/vercel.svg';
+
+interface ExternalLinkProps {
+  title: string;
+  handler: () => void;
+  icon: string;
+  className: string;
+}
+
+const ExternalLink: FC<ExternalLinkProps> = ({ title, handler, icon, className }) => (
+  <div className={`${className} cursor-pointer`} onClick={handler} aria-hidden='true'>
+    <Image src={icon} alt={className} height={90} width={90} priority />
+    <div>{title}</div>
+  </div>
+);
 
 export const Home = () => {
   const goToNextJs = () => window.open('https://nextjs.org/', '_blank');
@@ -14,27 +32,19 @@ export const Home = () => {
   return (
     <Page title='React is pretty cool' showHero>
       <div className='home-container'>
-        <div className='w-[700px]'>
+        <div className='w-[700px] text-grayScale06'>
           I have recently started using Next.js and SWR for React projects. Vercel did a great job making things a
           little easier to implement React Apps. Some things I really like about the Next.js framework is that it
           handles routing and code splitting out of the box. Also, the Image component is really useful for displaying
-          assets like SVGs on a page. Goodbye webpack.config.js. Click the buttons below to learn more about using
-          Next.js, SWR, GraphQL, and tailwind.
+          assets like SVGs on a page. Goodbye webpack.config.js. Click below to learn more about Next.js, SWR, GraphQL,
+          and tailwind.
         </div>
         <div className='mt-14'>
-          <div className='flex flex-row space-x-10'>
-            <Button type='primary' size='large' onClick={goToNextJs}>
-              Next.js
-            </Button>
-            <Button type='primary' size='large' onClick={goToSWR}>
-              SWR
-            </Button>
-            <Button type='primary' size='large' onClick={goToGraphQL}>
-              GraphQL
-            </Button>
-            <Button type='primary' size='large' onClick={goToTailwind}>
-              Tailwind
-            </Button>
+          <div className='flex flex-row space-x-20 text-center'>
+            <ExternalLink title='Next.js' handler={goToNextJs} icon={VercelIcon} className='next' />
+            <ExternalLink title='SWR' handler={goToSWR} icon={SWRIcon} className='swr' />
+            <ExternalLink title='GraphQL' handler={goToGraphQL} icon={GraphQLIcon} className='graphql' />
+            <ExternalLink title='Tailwind' handler={goToTailwind} icon={TailwindIcon} className='tailwind' />
           </div>
         </div>
       </div>
