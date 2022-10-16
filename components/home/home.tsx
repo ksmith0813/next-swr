1da7e7750c02f9ca98c49fce593402b7fccac160
Kevin Smith <ksmith0813@gmail.com>
2022-10-16 10:33:48 -0500

    added animation on hover for external links

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -16,7 +16,7 @@ interface ExternalLinkProps {
 }
 
 const ExternalLink: FC<ExternalLinkProps> = ({ title, handler, icon, className, size = 90, top = '' }) => (
-  <div className={`${className} ${top} cursor-pointer`} onClick={handler} aria-hidden='true'>
+  <div className={`external-link cursor-pointer ${className} ${top}`} onClick={handler} aria-hidden='true'>
     <Image src={icon} alt={className} height={size} width={size} priority />
     <div className={top}>{title}</div>
   </div>
