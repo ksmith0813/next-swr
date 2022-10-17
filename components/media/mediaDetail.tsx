3ad472cbef5fd1bc7f9735f8d54a1ac478e7e6fd
Kevin Smith <ksmith0813@gmail.com>
2022-10-17 11:32:59 -0500

    cleaned up media content for a cleaner return JSX element

--- components/media/mediaDetail.tsx
+++ components/media/mediaDetail.tsx
@@ -66,79 +66,89 @@ export const MediaDetail = () => {
 
   const hasRatings = selectedMedia?.Ratings?.length > 0;
 
-  const mediaDetailContent = (
-    <div className='bg-white'>
-      <Row className='border-b border-grayScale03 p-4' align='middle'>
-        <Col className='text-2xl' flex={1}>
-          {selectedMedia.Title}
-        </Col>
-        <Col>
-          <Button type='primary' onClick={() => backToAll()}>
-            Back to All
-          </Button>
-        </Col>
-      </Row>
-      <Row className='p-6'>
-        <Col>
-          {selectedMedia.Poster !== 'N/A' && (
-            <Image src={selectedMedia.Poster} height={450} width={300} alt='' priority />
-          )}
-          {selectedMedia.Poster === 'N/A' && <NoData message='Poster not available' />}
-        </Col>
-        <Col span={9} className='pl-6'>
-          <DataItem label='Plot'>{selectedMedia.Plot}</DataItem>
-          <DataItem label='Release Year' labelClasses='pt-4'>
-            {moment(selectedMedia.Released).format('MM/DD/YYYY')}
-          </DataItem>
-          <DataItem label='Director' labelClasses='pt-4'>
-            {selectedMedia.Director}{' '}
-          </DataItem>
-          <DataItem label='Actors' labelClasses='pt-4'>
-            {selectedMedia.Actors}
-          </DataItem>
-          <DataItem label='Writers' labelClasses='pt-4'>
-            {selectedMedia.Writer}
-          </DataItem>
-          <DataItem label='Genre' labelClasses='pt-4'>
-            {selectedMedia?.Genre?.split(',').map((g) => (
-              <Tag key={g}>{g}</Tag>
-            ))}
-          </DataItem>
-          <DataItem label='Rated' labelClasses='pt-4'>
-            {selectedMedia.Rated}
-          </DataItem>
-        </Col>
-        <Col span={8} className='pl-6'>
-          {hasRatings && (
-            <>
-              <b>Ratings</b>
-              {selectedMedia.Ratings.map((r: RatingsProps) => (
-                <DataItem
-                  key={r.Source}
-                  label={r.Source === 'Internet Movie Database' ? 'IMDB' : r.Source}
-                  labelClasses='pt-2 text-grayScale06'>
-                  <Progress percent={getRating(r.Value)} />
-                </DataItem>
-              ))}
-            </>
-          )}
-          <DataItem label='Box Office' labelClasses={hasRatings ? 'pt-2' : ''} childrenClasses='text-xl'>
-            {selectedMedia.BoxOffice}
-          </DataItem>
-          <DataItem label='Awards' labelClasses='pt-4'>
-            {selectedMedia.Awards}
-          </DataItem>
-          <DataItem label='Runtime' labelClasses='pt-4'>
-            {selectedMedia.Runtime}
-          </DataItem>
-        </Col>
-      </Row>
-    </div>
+  const headerContent = (
+    <Row className='border-b border-grayScale03 p-4' align='middle'>
+      <Col className='text-2xl' flex={1}>
+        {selectedMedia.Title}
+      </Col>
+      <Col>
+        <Button type='primary' onClick={() => backToAll()}>
+          Back to All
+        </Button>
+      </Col>
+    </Row>
+  );
+
+  const posterContent = (
+    <Col>
+      {selectedMedia.Poster !== 'N/A' && <Image src={selectedMedia.Poster} height={450} width={300} alt='' priority />}
+      {selectedMedia.Poster === 'N/A' && <NoData message='Poster not available' />}
+    </Col>
+  );
+
+  const detailContent = (
+    <Col span={9} className='pl-6'>
+      <DataItem label='Plot'>{selectedMedia.Plot}</DataItem>
+      <DataItem label='Release Year' labelClasses='pt-4'>
+        {moment(selectedMedia.Released).format('MM/DD/YYYY')}
+      </DataItem>
+      <DataItem label='Director' labelClasses='pt-4'>
+        {selectedMedia.Director}{' '}
+      </DataItem>
+      <DataItem label='Actors' labelClasses='pt-4'>
+        {selectedMedia.Actors}
+      </DataItem>
+      <DataItem label='Writers' labelClasses='pt-4'>
+        {selectedMedia.Writer}
+      </DataItem>
+      <DataItem label='Genre' labelClasses='pt-4'>
+        {selectedMedia?.Genre?.split(',').map((g) => (
+          <Tag key={g}>{g}</Tag>
+        ))}
+      </DataItem>
+      <DataItem label='Rated' labelClasses='pt-4'>
+        {selectedMedia.Rated}
+      </DataItem>
+    </Col>
+  );
+
+  const ratingsContent = (
+    <Col span={8} className='pl-6'>
+      {hasRatings && (
+        <>
+          <b>Ratings</b>
+          {selectedMedia.Ratings.map((r: RatingsProps) => (
+            <DataItem
+              key={r.Source}
+              label={r.Source === 'Internet Movie Database' ? 'IMDB' : r.Source}
+              labelClasses='pt-2 text-grayScale06'>
+              <Progress percent={getRating(r.Value)} />
+            </DataItem>
+          ))}
+        </>
+      )}
+      <DataItem label='Box Office' labelClasses={hasRatings ? 'pt-2' : ''} childrenClasses='text-xl'>
+        {selectedMedia.BoxOffice}
+      </DataItem>
+      <DataItem label='Awards' labelClasses='pt-4'>
+        {selectedMedia.Awards}
+      </DataItem>
+      <DataItem label='Runtime' labelClasses='pt-4'>
+        {selectedMedia.Runtime}
+      </DataItem>
+    </Col>
   );
 
   return (
     <Page title={TITLES.media} route={ROUTES.mediaDetail}>
-      {mediaDetailContent}
+      <div className='bg-white'>
+        {headerContent}
+        <Row className='p-6'>
+          {posterContent}
+          {detailContent}
+          {ratingsContent}
+        </Row>
+      </div>
     </Page>
   );
 };
